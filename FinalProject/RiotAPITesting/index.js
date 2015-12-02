714f7c8bc1034d769246f690dc965dd22a27df40
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-02 12:05:51 -0700

    Parse data

--- FinalProject/RiotAPITesting/index.js
+++ FinalProject/RiotAPITesting/index.js
@@ -28,12 +28,11 @@ app.post('/match',function(req,res){
 	 if(!err && response.statusCode < 400){
       context.summoner = JSON.parse(body);
 	  console.log(context.summoner);
-	  var summonerId = context.summoner.id;
-	  request('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/' + summonerId + '?api_key=05d6825e-a0c3-40e7-bdfa-475b4d8d7b56', function(err, reponse, body) {
-		  context.match = body;
-		  //console.log(context.match);
-	  });
-	 
+	  var bParams = [];
+	  for (var p in body) {
+		  bParams.push({'name':p, 'id':body[b]})
+	  }
+	  console.log(bParams);
     } else {
      if(response){
        console.log(response.statusCode);
