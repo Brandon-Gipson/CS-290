328abb072c47aaaa0dcbd996666c71af57417644
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-01 19:23:37 -0700

    Riot API Test

---
+++ FinalProject/RiotAPITesting/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3001);
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.post('/matchhistory',function(req,res){
+	var context = [];
+	if(req.body['summoner']){
+	request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+ req.body.summonerName +'?api_key=05d6825e-a0c3-40e7-bdfa-475b4d8d7b56', function(err, response, body){
+	 if(!err && response.statusCode < 400){
+      context = JSON.parse(body);
+	  console.log(context);
+	 
+    } else {
+      if(response){
+        console.log(response.statusCode);
+      }
+      next(err);
+	}
+	});
+  res.render('matchhistory', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
