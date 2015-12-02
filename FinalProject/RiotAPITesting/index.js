beb4b01660c3d9ca176fe04eecf0f5afc9ab5c87
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-01 20:31:18 -0700

    sad

--- FinalProject/RiotAPITesting/index.js
+++ FinalProject/RiotAPITesting/index.js
@@ -26,10 +26,10 @@ app.post('/match',function(req,res){
 	if(req.body['summoner']){
 	request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+ req.body.name +'?api_key=05d6825e-a0c3-40e7-bdfa-475b4d8d7b56', function(err, response, body){
 	 if(!err && response.statusCode < 400){
-      context.summoner = JSON.parse(body);
+      context.summoner = body;
 	  console.log(context.summoner);
 	  request('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/' + JSON.stringify(context.summoner.id) + '?api_key=05d6825e-a0c3-40e7-bdfa-475b4d8d7b56', function(err, reponse, body) {
-		  context.match = JSON.parse(body);
+		  context.match = body;
 		  console.log(context.match);
 	  });
 	 
