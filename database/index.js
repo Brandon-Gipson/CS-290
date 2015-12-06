19a0e88b49d1bde5aef6ce0d8f0d2be58eae3069
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-06 16:52:03 -0700

    fa

--- database/index.js
+++ database/index.js
@@ -78,7 +78,6 @@ app.get('/update',function(req,res,next){
       next(err);
       return;
     }
-    if(result.length == 1){
       var curVals = result[0];
       mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
         [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
@@ -90,7 +89,6 @@ app.get('/update',function(req,res,next){
         context.results = "Updated " + result.changedRows + " rows.";
         res.render('update',context);
       });
-    }
   });
 });
 
