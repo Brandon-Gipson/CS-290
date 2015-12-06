ba595d022062c45b028e4bd43848685d64cd21c9
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-06 13:21:57 -0700

    Add table creation

--- database/index.js
+++ database/index.js
@@ -11,6 +11,23 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    [your connection pool].query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
 app.get('/',function(req,res,next){
   var context = {};
   mysql.pool.query('SELECT * FROM workouts', funtion(err, rows, fields){
