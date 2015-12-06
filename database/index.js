c102ecd5b382efd5ef32e5cfa30c396a44085164
Brandon Gipson <gipsonb@oregonstate.edu>
2015-12-06 16:09:32 -0700

    gsf

--- database/index.js
+++ database/index.js
@@ -73,6 +73,7 @@ app.post('/',function(req,res){
 app.post('/update',function(req,res,next){
   var context = {};
   mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.body.id], function(err, result){
+	  console.log(result);
     if(err){
       next(err);
       return;
